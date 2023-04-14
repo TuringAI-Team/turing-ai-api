121ec5bdcd6d7d60eafac36704387ab1160644d1
MrlolDev <mrlol.yt.oficial@gmail.com>
2023-04-14 06:07:27 +0200

    fix

--- src/routes/cache.routes.ts
+++ src/routes/cache.routes.ts
@@ -11,16 +11,20 @@ import {
 const router = express.Router();
 
 router.post("/savecache", async (req: Request, res: Response) => {
+  console.log("svae cache");
   let { message, response, model } = req.body;
   await saveInCache(message, response, model);
   res.json({ success: true }).status(200);
 });
 router.post("/checkcache", async (req: Request, res: Response) => {
+  console.log("check cache");
   let { message, model } = req.body;
   let response = await checkInCache(message, model);
   res.json({ success: true, response: response }).status(200);
 });
 router.post("/addusescache", async (req: Request, res: Response) => {
+  console.log("use cache");
+
   let { message, model } = req.body;
   await addUsesInCache(message, model);
   res.json({ success: true }).status(200);
