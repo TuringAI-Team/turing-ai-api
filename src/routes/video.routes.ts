e31fc1b881102f3ee842b69701833ad926141b02
MrlolDev <mrlol.yt.oficial@gmail.com>
2023-05-06 14:24:41 +0200

    fix: logs

--- src/routes/video.routes.ts
+++ src/routes/video.routes.ts
@@ -8,28 +8,33 @@ import generateVideo2 from "../modules/video/videocrafter.js";
 const router = express.Router();
 
 router.post("/:m", key, turnstile, async (req: Request, res: Response) => {
-  let { m } = req.params;
-  if (m == "damo") {
-    let {
-      prompt,
-      num_frames = 50,
-      num_inference_steps = 50,
-      fps = 8,
-    } = req.body;
-    if (!prompt) return res.json({ error: "No prompt provided" }).status(400);
-    let result = await generateVideo(
-      prompt,
-      num_frames,
-      num_inference_steps,
-      fps
-    );
-    res.json(result).status(200);
-  }
-  if (m == "videocrafter") {
-    let { prompt, ddim_steps = 50, lora_model = "None" } = req.body;
-    if (!prompt) return res.json({ error: "No prompt provided" }).status(400);
-    let result = await generateVideo2(prompt, ddim_steps, lora_model);
-    res.json(result).status(200);
+  try {
+    let { m } = req.params;
+    if (m == "damo") {
+      let {
+        prompt,
+        num_frames = 50,
+        num_inference_steps = 50,
+        fps = 8,
+      } = req.body;
+      if (!prompt) return res.json({ error: "No prompt provided" }).status(400);
+      let result = await generateVideo(
+        prompt,
+        num_frames,
+        num_inference_steps,
+        fps
+      );
+      res.json(result).status(200);
+    }
+    if (m == "videocrafter") {
+      let { prompt, ddim_steps = 50, lora_model = "None" } = req.body;
+      if (!prompt) return res.json({ error: "No prompt provided" }).status(400);
+      let result = await generateVideo2(prompt, ddim_steps, lora_model);
+      res.json(result).status(200);
+    }
+  } catch (err: any) {
+    console.log(`err video: ${JSON.stringify(err)}}`);
+    res.json({ error: err, success: false }).status(400);
   }
 });
 
