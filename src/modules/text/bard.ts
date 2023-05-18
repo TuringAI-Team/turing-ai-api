69474aa21e84dfa5d0499044640a0f8150a366cf
MrlolDev <mrlol.yt.oficial@gmail.com>
2023-05-18 21:25:04 +0200

    fix: error handling

--- src/modules/text/bard.ts
+++ src/modules/text/bard.ts
@@ -17,75 +17,81 @@ export default async function bard(message, conversationId) {
   if (!acc) return { error: "max-accs-reached" };
   await addMsg(acc);
   let cookies = `__Secure-1PSID=${acc.token}`;
-  let bot = new Bard(cookies, {
-    inMemory: false, // optional: if true, it will not save conversations to disk
-    savePath: "./temp/conversations.json", // optional: path to save conversations
-    proxy: {
-      // optional: proxy configuration
-      host: process.env.PROXY_HOST,
-      port: 80,
-      auth: {
-        username: process.env.PROXY_USERNAME,
-        password: process.env.PROXY_PASSWORD,
-      },
-      protocol: "http",
-    },
-  });
-  let { data: conversation }: any = await supabase
-    .from("conversations_new")
-    .select("*")
-    .eq("id", conversationId);
-  var response;
-  conversation = conversation[0];
-  response = await bot.ask(message, conversationId); // conversationId is optional
-  let newConversations = await bot.getAllConversations();
-  // get the actual conversation
-  let actualConversation = newConversations.find((x) => x.id == conversationId);
-  if (conversation) {
-    await supabase.from("conversations_new").update({
-      history: {
-        data: {
-          ...actualConversation,
+  try {
+    let bot = new Bard(cookies, {
+      inMemory: false, // optional: if true, it will not save conversations to disk
+      savePath: "./temp/conversations.json", // optional: path to save conversations
+      proxy: {
+        // optional: proxy configuration
+        host: process.env.PROXY_HOST,
+        port: 80,
+        auth: {
+          username: process.env.PROXY_USERNAME,
+          password: process.env.PROXY_PASSWORD,
         },
-        messages: [
-          ...conversation.history.messages,
-          {
-            message: message,
-            role: "user",
-          },
-          {
-            role: "assistant",
-            message: response,
-          },
-        ],
+        protocol: "http",
       },
     });
-  } else {
-    await supabase.from("conversations_new").insert({
-      history: {
-        data: {
-          ...actualConversation,
+    let { data: conversation }: any = await supabase
+      .from("conversations_new")
+      .select("*")
+      .eq("id", conversationId);
+    var response;
+    conversation = conversation[0];
+    response = await bot.ask(message, conversationId); // conversationId is optional
+    let newConversations = await bot.getAllConversations();
+    // get the actual conversation
+    let actualConversation = newConversations.find(
+      (x) => x.id == conversationId
+    );
+    if (conversation) {
+      await supabase.from("conversations_new").update({
+        history: {
+          data: {
+            ...actualConversation,
+          },
+          messages: [
+            ...conversation.history.messages,
+            {
+              message: message,
+              role: "user",
+            },
+            {
+              role: "assistant",
+              message: response,
+            },
+          ],
         },
-        messages: [
-          {
-            message: message,
-            role: "user",
+      });
+    } else {
+      await supabase.from("conversations_new").insert({
+        history: {
+          data: {
+            ...actualConversation,
           },
-          {
-            role: "assistant",
-            message: response,
-          },
-        ],
-      },
-      id: conversationId,
-      tone: null,
-      model: "bard",
-    });
+          messages: [
+            {
+              message: message,
+              role: "user",
+            },
+            {
+              role: "assistant",
+              message: response,
+            },
+          ],
+        },
+        id: conversationId,
+        tone: null,
+        model: "bard",
+      });
+    }
+
+    await removeMsg(acc);
+
+    return { response };
+  } catch (e) {
+    await removeMsg(acc);
   }
-
-  await removeMsg(acc);
-
-  return { response };
 }
 
 async function addMsg(acc) {
