8dced0383f548289e248a940f6e7bb6272d26a5a
MrlolDev <mrlol.yt.oficial@gmail.com>
2023-05-19 07:26:51 +0200

    fix: sort

--- src/modules/text/bard.ts
+++ src/modules/text/bard.ts
@@ -8,7 +8,7 @@ export async function getAcc() {
   // sort by messages
   accs.sort((a, b) => a.messages - b.messages);
   // sort by total messages
-  accs.sort((a, b) => a.totalMessages - b.totalMessages);
+  // accs.sort((a, b) => a.totalMessages - b.totalMessages);
   // get the first one
   // get one of the first 3
   accs = accs.slice(0, 3);
