ec083fd9e92d598f7793150f439478c9eb64ebea
MrlolDev <mrlol.yt.oficial@gmail.com>
2023-05-27 20:01:56 +0200

    fix: channel bug

--- src/modules/image/midjourney.ts
+++ src/modules/image/midjourney.ts
@@ -1,19 +1,25 @@
 import client from "../../selfbot.js";
-import { CategoryChannel, Client, TextChannel } from "discord.js-selfbot-v13";
+import {
+  CategoryChannel,
+  Client,
+  MessageButton,
+  MessageSelectMenu,
+  TextChannel,
+} from "discord.js-selfbot-v13";
 import EventEmitter from "events";
 
-let generating = 0;
-let describing = 0;
+let generating = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
+let describing = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
 const botClient: Client = client;
 
 export async function imagine(prompt: string, model?: string) {
   let event = new EventEmitter();
   let guild = botClient.guilds.cache.get("1111700862868406383");
   if (!guild) return;
-  generating++;
+  let genAt = generating.pop();
   // get channel by name
   let channel = guild.channels.cache.find(
-    (x) => x.name == generating.toString()
+    (x) => x.name == genAt.toString()
   ) as TextChannel;
   if (!channel.isText()) return;
   // use application command
@@ -24,6 +30,7 @@ export async function imagine(prompt: string, model?: string) {
     status: null,
     done: false,
     credits: 0,
+    id: null,
   };
   switch (model) {
     case "5.1":
@@ -68,7 +75,7 @@ export async function imagine(prompt: string, model?: string) {
         //  each second is 0.001 credits
         let credits = timeInS * 0.001;
         data.credits = credits;
-        generating--;
+        generating.push(genAt);
         clearInterval(interval);
       }
     });
@@ -81,10 +88,10 @@ export async function describe(image: string) {
   let event = new EventEmitter();
   let guild = botClient.guilds.cache.get("1111700862868406383");
   if (!guild) return;
-  describing++;
+  let desAt = describing.pop();
   // get channel by name
   let channel = guild.channels.cache.find(
-    (x) => x.name == `d-${describing.toString()}`
+    (x) => x.name == `d-${desAt.toString()}`
   ) as TextChannel;
   if (!channel.isText()) return;
   // use application command
@@ -106,7 +113,7 @@ export async function describe(image: string) {
       data = x;
       event.emit("data", data);
       if (data.done) {
-        describing--;
+        describing.push(desAt);
         clearInterval(interval);
       }
     });
@@ -117,7 +124,9 @@ export async function describe(image: string) {
 }
 async function checkStatus(channel, user, data) {
   let x = await channel.messages.fetch();
-  let messages = x.filter((x) => x.author.id == user.id).first();
+  let messages = x
+    .filter((x) => x.author.id == user.id && x.content.includes(data.prompt))
+    .first();
   if (!messages) return;
   // get message content
   let content = messages.content;
@@ -130,7 +139,6 @@ async function checkStatus(channel, user, data) {
   if (status == "fast") {
     data.status = 1;
     data.done = true;
-    generating--;
   } else if (status == "Waiting to start") {
     data.status = 0;
   } else {
@@ -174,4 +182,59 @@ async function checkStatusDescribe(channel, user, data) {
   data.done = true;
 
   return data;
+}
+
+export async function variation(id: string, number = 1) {
+  let messageId = id.split("-")[0];
+  let channelid = parseInt(id.split("-")[1]);
+  let event = new EventEmitter();
+  let guild = botClient.guilds.cache.get("1111700862868406383");
+  if (!guild) return;
+  let channel = guild.channels.cache.find(
+    (x) => x.name == channelid.toString()
+  ) as TextChannel;
+  // remove channelid from generating array
+  generating = generating.filter((x) => x != channelid);
+  if (!channel.isText()) return;
+  if (!channel.isText()) return;
+  let message = await channel.messages.fetch(messageId);
+  let actionRows = message.components;
+  let variationRow = actionRows[1];
+  let button = variationRow.components[number - 1] as MessageButton;
+  // use application command
+  const user = botClient.users.cache.get("936929561302675456");
+  let data = {
+    prompt: message.content.split(" - ")[0],
+    image: null,
+    status: null,
+    done: false,
+    credits: 0,
+  };
+
+  let startTime = Date.now();
+  // get last message from bot in channel
+  await button.click(message);
+
+  checkStatus(channel, user, data).then((x) => {
+    data = x;
+    event.emit("data", data);
+  });
+  let interval = setInterval(() => {
+    checkStatus(channel, user, data).then((x) => {
+      data = x;
+      event.emit("data", data);
+      console.log(data);
+
+      if (data.done) {
+        let timeInS = (Date.now() - startTime) / 1000;
+        //  each second is 0.001 credits
+        let credits = timeInS * 0.001;
+        data.credits = credits;
+        generating.push(channelid);
+        clearInterval(interval);
+      }
+    });
+  }, 1000 * 3);
+
+  return event;
 }
