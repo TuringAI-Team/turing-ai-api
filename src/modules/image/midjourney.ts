0eb52c8ef082586043a747fc6bd4a67c20f9d52e
MrlolDev <mrlol.yt.oficial@gmail.com>
2023-05-27 21:56:04 +0200

    fix: upscale

--- src/modules/image/midjourney.ts
+++ src/modules/image/midjourney.ts
@@ -219,10 +219,7 @@ export async function buttons(id, action, number = 1) {
   let message = await channel.messages.fetch(messageId);
   let actionRows = message.components;
   let variationRow: any = actionRows[action == "upscale" ? 0 : 1];
-  console.log(
-    variationRow,
-    variationRow.components.find((x: any) => x.label == `U${number}`)
-  );
+
   let button = variationRow.components[number - 1];
   console.log(button);
   // use application command
