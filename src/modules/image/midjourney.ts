68346ab93a1d2157cbdcbd3b8ec350df69118b83
MrlolDev <mrlol.yt.oficial@gmail.com>
2023-05-31 07:05:57 +0200

    fix

--- src/modules/image/midjourney.ts
+++ src/modules/image/midjourney.ts
@@ -52,7 +52,10 @@ export async function imagineWithQueue(
     prompt: prompt,
   });
   let promptsGenerating = queue.filter((x) => x.generating);
-  console.log(promptsGenerating.length, queue.length);
+  console.log(
+    promptsGenerating.length,
+    queue.length - promptsGenerating.length
+  );
   // check queue, if it is the first one, start it with imagine
   await checkQueuePostion(queuePos, job, prompt, mode, model, event);
   let interval = setInterval(async () => {
