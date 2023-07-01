3212040d46062523922b9074fde0d238d36d8542
MrlolDev <mrlol.yt.oficial@gmail.com>
2023-07-01 15:46:50 +0200

    feat: auto generated sidebar && fix node_env

--- src/utils/log.ts
+++ src/utils/log.ts
@@ -6,11 +6,23 @@ const logsPath = "./logs";
 const sessionID = randomUUID();
 const logFile = `${logsPath}/${sessionID}.log`;
 
-export default function log(type: "info" | "warning" | "error", ...args) {
+export default function log(
+  type: "info" | "warning" | "error" | "debug",
+  ...args
+) {
   const log = `[${type.toUpperCase()}] ${args.join(" ")}`;
   const color =
-    type === "info" ? "green" : type === "warning" ? "yellow" : "red";
-  if (process.env.NODE_ENV == "development") {
+    type === "info"
+      ? "green"
+      : type === "warning"
+      ? "yellow"
+      : type === "error"
+      ? "red"
+      : "white";
+  if (
+    (process.env.NODE_ENV == "production" && type != "debug") ||
+    process.env.NODE_ENV == "development"
+  ) {
     console.log(chalk[color](log));
   }
   fs.appendFileSync(logFile, `${log}\n`);
