67d4352f13044a0fe41c12d67bd7734ad5733164
MrlolDev <mrlol.yt.oficial@gmail.com>
2023-07-02 17:41:07 +0200

    fix

--- src/models/text/gpt.ts
+++ src/models/text/gpt.ts
@@ -237,6 +237,7 @@ async function streams(data) {
           event.emit("data", result);
         } else {
           tokensSent++;
+          console.log(content);
           content = JSON.parse(content);
           let text = content.choices[0].delta.content;
           let finishReason = content.choices[0].finish_reason;
