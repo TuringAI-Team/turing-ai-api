eeb4f940915ee2bb501f6e826f2ca3dfd18386fd
MrlolDev <mrlol.yt.oficial@gmail.com>
2023-07-02 15:32:43 +0200

    fix

--- src/models/text/gpt.ts
+++ src/models/text/gpt.ts
@@ -199,7 +199,7 @@ async function streams(data) {
     let stream = response.data;
     stream.on("data", (chunk) => {
       let content = chunk.toString();
-      event.emit("data", content);
+      event.emit("data", content.trim());
     });
     stream.on("end", () => {
       event.emit("end");
