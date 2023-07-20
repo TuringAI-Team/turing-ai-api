2b7d705729e26828c6bb90fc0b60c58be19240d4
MrlolDev <mrlol.yt.oficial@gmail.com>
2023-07-20 18:39:32 +0200

    fix

---
+++ src/models/text/gpt-new.ts
@@ -0,0 +1,49 @@
+import delay from "delay";
+import EventEmitter from "events";
+import { Configuration, OpenAIApi } from "openai";
+import pluginList from "../../utils/plugins.js";
+import {
+  getChatMessageLength,
+  getPromptLength,
+} from "../../utils/tokenizer.js";
+import { fetchEventSource } from "@waylaidwanderer/fetch-event-source";
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+export default {
+  data: {
+    name: "gpt",
+    fullName: "OpenAI models",
+    parameters: {
+      messages: {
+        type: "array",
+        required: true,
+      },
+      model: {
+        type: "string",
+        required: true,
+        options: ["gpt-3.5-turbo", "gpt-4", "gpt-3.5-turbo-16k"],
+      },
+      max_tokens: {
+        type: "number",
+        required: false,
+        default: 512,
+      },
+      temperature: {
+        type: "number",
+        required: false,
+        default: 0.9,
+      },
+      plugins: {
+        type: "array",
+        required: false,
+        default: [],
+        options: pluginList.map((p) => p.name),
+      },
+    },
+  },
+  execute: async (data) => {},
+};
--- src/routes/other.routes.ts
+++ src/routes/other.routes.ts
@@ -52,7 +52,7 @@ async function convertToVideo(audio, image, duration, callback) {
     .input(audioPath)
     .input(imagePath)
     .outputOptions([
-      "-c:v libopenh264",
+      "-c:v libx264",
       "-c:a aac",
       "-shortest",
       "-vf scale=1280:720",
