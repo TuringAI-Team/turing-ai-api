34a9ee4db6bb3996137bd4611dc528ddaf7326be
MrlolDev <mrlol.yt.oficial@gmail.com>
2023-07-22 19:28:27 +0200

    fix

--- src/models/image/kandinsky.ts
+++ src/models/image/kandinsky.ts
@@ -161,6 +161,7 @@ export default {
           status: "success",
         });
       }
+      console.log(result);
       result.status = "done";
       result.progress = null;
       event.emit("data", result);
