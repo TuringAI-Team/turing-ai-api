8aec23662fa8fafd0f6e11196bac89ee66d90582
mrloldev <mrlol.yt.oficial@gmail.com>
2023-08-24 08:31:53 +0200

    fix: run ciclic just on production

--- src/utils/ciclic.ts
+++ src/utils/ciclic.ts
@@ -2,9 +2,11 @@ import { getUpdatedStats } from "./stats.js";
 import ms from "ms";
 
 export default async function ciclic() {
-  let guilds = await getUpdatedStats();
-  console.log(guilds);
-  setInterval(async () => {
-    guilds = await getUpdatedStats();
-  }, ms("1h"));
+  if (process.env.NODE_ENV == "production") {
+    let guilds = await getUpdatedStats();
+    console.log(guilds);
+    setInterval(async () => {
+      guilds = await getUpdatedStats();
+    }, ms("1h"));
+  }
 }
